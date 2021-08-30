d25fedbf68f179dadb70a967eb369558b0026a18
Heizel Kim <hrkim0624@gmail.com>
2021-08-31 04:03:11 +0900

    [SC-281] Add `test_json_ld_vcr.py`.

--- didsdk/protocol/json_ld/json_ld_vcr.py
+++ didsdk/protocol/json_ld/json_ld_vcr.py
@@ -15,3 +15,6 @@ class JsonLdVcr(BaseJsonLd):
     def from_(data: Dict[str, Any]) -> 'JsonLdVcr':
         return JsonLdVcr(context=data.get(PropertyName.JL_CONTEXT), id_=data.get(PropertyName.JL_ID),
                          type_=data.get(PropertyName.JL_AT_TYPE), request_claim=data.get(PropertyName.JL_REQUEST_CLAIM))
+
+    def get_request_claim(self, key: str):
+        return self.get_term(PropertyName.JL_REQUEST_CLAIM).get(key)
