0e28f58afd0a42a0fb5848532eaa603fe25f8942
Inwon Kim <inwonkim@iconloop.com>
2024-10-21 08:43:15 +0900

    register P-256K on initialization file

--- didsdk/protocol/protocol_message.py
+++ didsdk/protocol/protocol_message.py
@@ -4,12 +4,8 @@ import time
 from dataclasses import dataclass
 from typing import Optional, Union
 
-from cryptography.hazmat.primitives.asymmetric.ec import SECP256K1
 from joserfc import jwe
 from joserfc.jwk import JWKRegistry
-from joserfc.jws import JWSRegistry
-from joserfc.rfc7518.ec_key import CURVES_DSS, DSS_CURVES
-from joserfc.rfc7518.jws_algs import ECAlgModel
 from loguru import logger
 
 from didsdk.core.did_key_holder import DidKeyHolder
@@ -34,15 +30,6 @@ class SignResult:
     success: bool = False
     result: Optional[dict] = None
     fail_message: str = None
-
-
-def register_p_256k():
-    JWSRegistry.register(ECAlgModel("ES256K", "P-256K", 256))
-    DSS_CURVES["P-256K"] = SECP256K1
-    CURVES_DSS["P-256K"] = "secp256k1"
-
-
-register_p_256k()
 
 
 class ProtocolMessage:
@@ -129,7 +116,7 @@ class ProtocolMessage:
         return self._is_protected
 
     @property
-    def jwe(self) -> str:
+    def jwe(self) -> Optional[str]:
         return self._jwe
 
     @property
